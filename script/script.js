007b8a086caac28670f4f83f03de2d5aafcf1c1d
HangerThem <f.borisjuk@gmail.com>
2021-11-24 19:03:13 +0100

    First commit

---
+++ script/script.js
@@ -0,0 +1,20 @@
+const images = document.querySelectorAll('[data-image]')
+const imageOpen = document.querySelector('[data-image-slot]')
+const container = document.querySelector('[data-image-container]')
+const close = document.querySelector('[data-close]')
+
+console.log(images)
+
+images.forEach(image => {
+    const source = image.src
+    const alt = image.alt
+    image.addEventListener('click', () => {
+        imageOpen.src = source
+        imageOpen.alt = alt
+        container.style.display = 'block'
+    })
+});
+
+close.addEventListener('click', () => {
+    container.style.display = 'none'
+})
